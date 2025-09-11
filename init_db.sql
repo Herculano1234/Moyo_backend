c73b9db683c41a9558a610126909fef4b92ab423
Herculano1234 <herculanogantonio@gemail.com>
2025-09-11 13:02:45 +0100

    tentandoarranjar

--- init_db.sql
+++ init_db.sql
@@ -100,3 +100,13 @@ CREATE TABLE IF NOT EXISTS consultas (
     local VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+CREATE TABLE IF NOT EXISTS administradores_moyo (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  telefone VARCHAR(20),
+  foto_url TEXT,
+  data_nascimento DATE,
+  senha VARCHAR(100) NOT NULL,
+  data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
