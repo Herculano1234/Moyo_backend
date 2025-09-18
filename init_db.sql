3fcd4ab694ec59c45bb2d1db4f759358ff858ba2
Herculano1234 <herculanogantonio@gemail.com>
2025-09-18 15:32:09 +0100

    Add tabela exames

--- init_db.sql
+++ init_db.sql
@@ -88,7 +88,19 @@ CREATE TABLE IF NOT EXISTS administradores_hospital (
   hospital_id INT REFERENCES hospitais(id),
   data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
+CREATE TABLE IF NOT EXISTS exames (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    disponivel BOOLEAN DEFAULT true,
+    profissional_id INT REFERENCES profissionais(id),
+    paciente_id INT REFERENCES pacientes(id),
+    hospital_id INT REFERENCES hospitais(id),
+    data_hora TIMESTAMP NOT NULL,
+    status VARCHAR(20) DEFAULT 'agendado',
+    resultado TEXT,
+    observacoes TEXT,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 -- Criar tabela de consultas
 CREATE TABLE IF NOT EXISTS consultas (
     id SERIAL PRIMARY KEY,
